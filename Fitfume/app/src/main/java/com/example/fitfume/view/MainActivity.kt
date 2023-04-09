b3bf6880f3c4dd62faef25a41b5016ef7560f966
lh99j <grand2181@gmail.com>
2023-04-09 10:52:18 +0900

    ChatGPT BuildConfigField

--- Fitfume/app/src/main/java/com/example/fitfume/view/MainActivity.kt
+++ Fitfume/app/src/main/java/com/example/fitfume/view/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
+import com.example.fitfume.BuildConfig
 import com.example.fitfume.databinding.ActivityMainBinding
 import com.example.fitfume.model.GptContent
 import com.example.fitfume.network.ApiInterface
@@ -27,15 +28,11 @@ class MainActivity : AppCompatActivity() {
 
         binding.gptVm = viewModel
 
-
-
         binding.sendBtn.setOnClickListener {
             val model = "gpt-3.5-turbo"
-            val request = GptRequest(model, listOf(GptContent("user", "안녕?")))
+            val request = GptRequest(model, listOf(GptContent("user", "\"캐주얼 스타일 옷을 입는 40대 남성 직장인\" 향수 추천해줘. 근데 향수 이름은 한글로 말해줘. 그리고 기타 설명은 적지말고 \"브랜드 - 향수이름\" 으로 딱 하나만 알려줘.")))
 
             viewModel.getChatAnswer(request)
         }
-
-
     }
 }
--- Fitfume/app/src/main/java/com/example/fitfume/viewmodel/GptViewModel.kt
+++ Fitfume/app/src/main/java/com/example/fitfume/viewmodel/GptViewModel.kt
@@ -2,6 +2,7 @@ package com.example.fitfume.viewmodel
 
 import android.annotation.SuppressLint
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.example.fitfume.network.data.request.GptRequest
 import com.example.fitfume.repository.GptRepositoryImpl
@@ -9,6 +10,7 @@ import io.reactivex.rxkotlin.subscribeBy
 
 class GptViewModel : ViewModel(){
     private val gptRepository = GptRepositoryImpl()
+
 
     @SuppressLint("CheckResult")
     fun getChatAnswer(request: GptRequest){
