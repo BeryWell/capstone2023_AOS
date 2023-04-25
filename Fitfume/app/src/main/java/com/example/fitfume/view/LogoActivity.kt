c32c14bc32720bad41687df01d6c376a4c0fbe37
lh99j <grand2181@gmail.com>
2023-04-25 13:04:25 +0900

    app logo

---
+++ Fitfume/app/src/main/java/com/example/fitfume/view/LogoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fitfume.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fitfume.R
+
+class LogoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logo)
+    }
+}
