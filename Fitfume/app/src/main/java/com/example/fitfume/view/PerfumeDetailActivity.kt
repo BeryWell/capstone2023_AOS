9342ac43d54dc1c6d9f5252bb5d993b94a69ede3
lh99j <grand2181@gmail.com>
2023-05-26 17:14:59 +0900

    임시 커밋

---
+++ Fitfume/app/src/main/java/com/example/fitfume/view/PerfumeDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fitfume.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fitfume.R
+
+class PerfumeDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfume_detail)
+    }
+}
